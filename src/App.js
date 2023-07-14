63c10e20cefd4dfe506a72fe7b227335c8a88c96
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-07-15 04:05:47 +1000

    Adding navbar with disney logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
         </Routes>
