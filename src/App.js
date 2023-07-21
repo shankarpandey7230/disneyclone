366bacd261405590a15e7a089f685736fd2c022c
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-07-22 01:09:15 +1000

    adding detailbackground image, image title, play button, trailer, addList icon, subtitle, description and getting movies form db

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 const App = () => {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
           <Route path="/home" element={<Home />}></Route>
+          <Route path="/detail/:id" element={<Detail />}></Route>
         </Routes>
       </Router>
     </div>
